adda932b432268d66891f86dfc5c13dbd3b9c471
fundamental-group <emathstutorials@gmail.com>
2023-01-26 17:49:33 -0800

    Robomania Added

---
+++ ME - ROBOMANIA/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    //public float speed;
+    public float yForce;
+    public float xForce;
+    public float xDirection;
+
+    private Rigidbody2D enemyRigidBody;
+
+
+    void Start()
+    {
+        enemyRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            Vector2 jumpForce = new Vector2(xForce * xDirection, yForce);
+            enemyRigidBody.AddForce(jumpForce);
+        }
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        /*
+         * Original Contents
+        if (transform.position.x <= -8 || transform.position.x >= 8)
+        {
+            speed = speed * -1;
+        }
+
+        float newXPosition = transform.position.x + speed * Time.deltaTime;
+        float newYPosition = transform.position.y;
+        Vector2 newPosition = new Vector2(newXPosition, newYPosition);
+        transform.position = newPosition;
+        */
+
+        if (transform.position.x <= -8)
+        {
+            xDirection = 1;
+            enemyRigidBody.AddForce(Vector2.right * xForce);
+        }
+
+        if (transform.position.x >= 8)
+        {
+            xDirection = -1;
+            enemyRigidBody.AddForce(Vector2.left * xForce);
+        }
+    }
+}
